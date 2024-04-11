55659e959ec3e9305f45399a45079f4533a895d8
DianaSemenova <menestrelia199702@gmail.com>
2024-04-11 21:53:49 +0400

    sass and react-router-dom installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App__header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App__link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
