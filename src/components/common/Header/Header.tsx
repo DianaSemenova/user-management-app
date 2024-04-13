bab1d5a43874b51183eca039a7248ed9dbe4315d
DianaSemenova <menestrelia199702@gmail.com>
2024-04-13 20:05:28 +0400

    feat: completed header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { IconButton, useMediaQuery } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
+  const isMobile = useMediaQuery("(max-width: 767px)");
+  const [isActiveNavBtn, setIsActiveBtn] = useState(false);
+
+  const handleClickNavBtn = () => {
+    if (isMobile) {
+      setIsActiveBtn(!isActiveNavBtn);
+    }
+  };
+
   return (
     <header className="header">
       <nav className="header-nav center">
         <Link to="/" className="header__link">
           My Application
         </Link>
-        <div className="header-nav-block">
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{
+            color: "rgba(255, 255, 255, 0.5)",
+            borderRadius: "4px",
+            border: "1px solid rgba(255, 255, 255, 0.1)",
+            padding: "4px 12px",
+            display: isMobile ? "flex" : "none",
+          }}
+          onClick={handleClickNavBtn}
+        >
+          <MenuIcon sx={{ fontSize: 30 }} />
+        </IconButton>
+        <div
+          className={
+            isActiveNavBtn ? "header-nav-block-mob" : "header-nav-block"
+          }
+          onClick={handleClickNavBtn}
+        >
           <Link to="/" className="header-nav__link">
             Home
           </Link>
